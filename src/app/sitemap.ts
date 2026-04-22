27145f0d8304589de2da1d97ff1980f4e7062dc8
fapethedev <mpetrivlin@gmail.com>
2026-04-22 19:36:51 +0000

    Update: Add alternates links as page are intl

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,27 +1,45 @@
 import {MetadataRoute} from "next";
+import { getPathname } from "@/i18n/navigation"
 
+const BASE_URL = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
 
-export default function sitemap(): MetadataRoute.Sitemap {
-    const BASE_URL = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
-
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     return [
         {
             url: BASE_URL,
             lastModified: new Date(),
             changeFrequency: "weekly",
             priority: 1,
+            alternates: {
+                languages: {
+                    en: BASE_URL + (getPathname({ locale: "en", href: "/" })),
+                    fr: BASE_URL + (getPathname({ locale: "fr", href: "/" }))
+                }
+            }
         },
         {
             url: `${BASE_URL}/scanner`,
             lastModified: new Date(),
             changeFrequency: "weekly",
             priority: 1,
+            alternates: {
+                languages: {
+                    en: BASE_URL + (getPathname({ locale: "en", href: "/scanner" })),
+                    fr: BASE_URL + (getPathname({ locale: "fr", href: "/scanner" }))
+                }
+            }
         },
         {
             url: `${BASE_URL}/about`,
             lastModified: new Date(),
             changeFrequency: "weekly",
             priority: 1,
+            alternates: {
+                languages: {
+                    en: BASE_URL + (getPathname({ locale: "en", href: "/about" })),
+                    fr: BASE_URL + (getPathname({ locale: "fr", href: "/about" }))
+                }
+            }
         },
     ]
 }
